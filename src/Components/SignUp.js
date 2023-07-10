9fc955dcabff71a995bc8266a03400854eff0e8f
Syed Mustassim <smustassim@gmail.com>
2023-07-10 17:20:16 +0530

    sign up fix

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,33 +1,81 @@
 import { Link } from "react-router-dom";
 import "./Login.css";
 import { useAuth } from "../Context/AuthContext";
+import { useState } from "react";
 
 export const SignUp = () => {
-  const { setActiveAuth } = useAuth();
+  const { setActiveAuth, signUpHandler } = useAuth();
 
   const authHandler = () => {
     setActiveAuth(true);
   };
 
+  const [signUp, setSignUp] = useState({
+    name: "",
+    username: "",
+    password: "",
+    confirmPassword: "",
+    profileImg: "https://i.postimg.cc/fTYff9yW/urahichi.gif",
+    backgroundImg: "https://i.postimg.cc/bYkXBpxM/mostCig.gif",
+  });
+
+  const signUpInput = (event) => {
+    if (event.target.name === "confirmPassword") {
+      setSignUp({
+        ...signUp,
+        [event.target.name]: event.target.value,
+      });
+    }
+  };
+
+  const signUpSubmit = (event) => {
+    event.preventDefault();
+    signUpHandler(signUp);
+  };
+
   return (
     <div>
       <div className="form-box register">
-        <form action="#">
+        <form onSubmit={signUpSubmit} action="#">
           <h2 className="register-heading"> Sign Up </h2>
           <div className="input-box">
-            <input type="text" required />
+            <input
+              type="text"
+              name="name"
+              value={signUp.name}
+              onChange={signUpInput}
+              required
+            />
             <label> Name </label>
           </div>
           <div className="input-box">
-            <input type="email" required />
-            <label> Email </label>
+            <input
+              type="text"
+              name="username"
+              value={signUp?.username}
+              onChange={signUpInput}
+              required
+            />
+            <label> Username </label>
           </div>
           <div className="input-box">
-            <input type="password" required />
+            <input
+              type="password"
+              name="password"
+              value={signUp?.password}
+              onChange={signUpInput}
+              required
+            />
             <label> Password </label>
           </div>
           <div className="input-box">
-            <input type="password" required />
+            <input
+              type="password"
+              name="confirmPassword"
+              value={signUp?.confirmPassword}
+              onChange={signUpInput}
+              required
+            />
             <label> Confirm Password </label>
           </div>
           <button type="submit" className="login-btn">
